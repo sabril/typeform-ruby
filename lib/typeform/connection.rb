4697120453c89b750bb4a0ed227de9351e7b6e46
Syaiful Sabril <syaiful.sabril@gmail.com>
2015-11-05 16:53:32 +0700

    updates

--- lib/typeform/connection.rb
+++ lib/typeform/connection.rb
@@ -1,7 +1,7 @@
 module Typeform
   class Connection
     BASE_URI = "https://api.typeform.com"
-    TYPEFORM_API_VERSION = "v0.4"
+    TYPEFORM_API_VERSION = "v0"
 
     def initialize(api_key)
       @api_key = api_key
