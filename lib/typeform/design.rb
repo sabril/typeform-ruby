7baccfad631f4f7b5c7390bebb18a39d62fb5da9
Syaiful Sabril <syaiful.sabril@gmail.com>
2016-01-10 00:14:49 +0700

    set attributes

--- lib/typeform/design.rb
+++ lib/typeform/design.rb
@@ -3,33 +3,32 @@ require "json-encodable"
 module Typeform
   class Design
     include JSON::Encodable
-
     property :colors, type: Hash
 
-    attr_accessor :colors
+    attr_accessor :question_color, :answer_color, :button_color, :background_color, :colors
 
-    def question
+    def question_color
       @question_color ||= "#3D3D3D"
     end
 
-    def answer
+    def answer_color
       @answer_color ||= "#4FB0AE"
     end
 
-    def button
+    def button_color
       @button_color ||= "#4FB0AE"
     end
 
-    def background
+    def background_color
       @background_color ||= "#FFFFFF"
     end
 
     def colors
       {
-        "question" => question,
-        "button" => answer,
-        "answer" => button,
-        "background" => background
+        "question" => question_color,
+        "button" => answer_color,
+        "answer" => button_color,
+        "background" => background_color
       }
     end
   end
